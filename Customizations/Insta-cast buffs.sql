8c149538e59031798f2e7a28cac0a47fc877b7e6
tegstewart <tegstewart@gmail.com>
2017-11-06 20:25:28 -0700

    Moved to Customizations

---
+++ Customizations/Insta-cast buffs.sql
@@ -0,0 +1,7 @@
+# Changes most buff spells to insta-cast
+# Note that this will make it much easier in PVP
+#	to rebuff after having buffs sheared.
+
+UPDATE `spell` SET `casttime`=0 WHERE `Type` LIKE '%buff%' AND `Concentration`>0;
+UPDATE `spell` SET `casttime`=0 WHERE `Type` LIKE '%buff%' AND `Type`!='HealthRegenBuff' AND `duration`>=600;
+UPDATE `spell` SET `casttime`=0 WHERE `TYPE`='petspell' AND `Duration`='1200'; # Necro spec buffs
---
+++ Customizations/SolidRunSpeedIcon.sql
@@ -0,0 +1,2 @@
+﻿# Changes run speed songs to give solid rather than flashing icons when active
+UPDATE `spell` SET `Duration`=18 WHERE `Type`='SpeedEnhancement' AND `Pulse`=1;
