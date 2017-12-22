639accccbe086625b1e2bb6fb1fa29653290645e
Lucas Falk <LRFalk01@gmail.com>
2017-12-21 21:03:09 -0500

    Update to support the new dragon scale loot generators (#70)
    * Update to support the new dragon scale loot generators
    
    closes #65
    thanks @tegstewart
    
    * Add missing faction for dragon scale loot

---
+++ src/data/LinkedFaction.sql
@@ -0,0 +1,7 @@
+INSERT INTO `LinkedFaction` (`FactionID`, `LinkedFactionID`, `IsFriend`, `LastTimeRowUpdated`, `LinkedFaction_ID`) VALUES
+	(97, 195, 0, '2000-01-01 00:00:00', 'Azure_Dragonsworn'),
+	(97, 83, 0, '2000-01-01 00:00:00', 'Azure_GlimmerSpirits'),
+	(124, 195, 0, '2000-01-01 00:00:00', 'DragonBaneSvaltalfar_Dragonsworn'),
+	(124, 154, 0, '2000-01-01 00:00:00', 'DragonbaneSvaltalfar_PackDrakulv'),
+	(60, 195, 0, '2000-01-01 00:00:00', 'StonecrushClan_Dragonsworn'),
+	(60, 31, 0, '2000-01-01 00:00:00', 'StonecrushClan_GraniteGiants');
