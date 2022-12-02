d53fd0188cac8e2efbc4ac8b66d59763efa85b0a
NetDwarf <NetDwarf@users.noreply.github.com>
2022-12-02 17:15:51 +0100

    Add table definitions in code
    Remove corresponding sql files
    Replace usage of sql files with built-in tables

--- schema_sqlite/TownCrierMessages.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `TownCrierMessages`;
-
-CREATE TABLE `TownCrierMessages` (`TownCrierID` VARCHAR(255) NOT NULL DEFAULT '' COLLATE NOCASE, 
-`Message` TEXT DEFAULT NULL COLLATE NOCASE, 
-`LastTimeRowUpdated` DATETIME NOT NULL DEFAULT '2000-01-01 00:00:00', 
-`TownCrierMessages_ID` VARCHAR(255) DEFAULT NULL COLLATE NOCASE, 
-PRIMARY KEY (`TownCrierID`));
-CREATE UNIQUE INDEX `U_TownCrierMessages_TownCrierMessages_ID` ON `TownCrierMessages` (`TownCrierMessages_ID`);
--- sql_provider.go
+++ sql_provider.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -13,6 +14,7 @@ type dbProvider interface {
 	getAllTables() *sql.Rows
 	getPrimaryKeyColumn(tableName string) string
 	closeConnection()
+	getCreateStatement(table Table) string
 }
 
 type sqliteProvider struct {
@@ -58,6 +60,76 @@ func (provider *sqliteProvider) getPrimaryKeyColumn(tableName string) string {
 
 func (provider *sqliteProvider) closeConnection() {
 	provider.db.Close()
+}
+
+func (provider *sqliteProvider) getCreateStatement(table Table) string {
+	stmt := ""
+	if table.Static {
+		stmt += fmt.Sprintf("DROP TABLE IF EXISTS `%v`;\n\n", table.Name)
+		stmt += fmt.Sprintf("CREATE TABLE `%v` (", table.Name)
+	} else {
+		stmt += fmt.Sprintf("CREATE TABLE IF NOT EXISTS `%v` (", table.Name)
+	}
+	for _, c := range table.Columns {
+		sqlTypeIsNumber := strings.Contains(c.SqlType, "int(") || c.SqlType == "double"
+		sqlTypeIsText := strings.HasPrefix(c.SqlType, "varchar") || c.SqlType == "text"
+		columnIsPrimaryKey := c.Name == table.PrimaryColumn.Name
+		if c.Name == table.PrimaryColumn.Name && sqlTypeIsNumber && table.AutoIncrement > 0 {
+			stmt += "`" + c.Name + "` INTEGER"
+		} else {
+			sqlType := strings.Split(c.SqlType, " ")
+			if len(sqlType) == 2 { //int(11) unsigned -> unsigned int(11)
+				sqlType = []string{sqlType[1], sqlType[0]}
+			}
+			stmt += fmt.Sprintf("`%v` %v", c.Name, strings.ToUpper(strings.Join(sqlType, " ")))
+		}
+		if c.NotNull {
+			stmt += " NOT NULL"
+		}
+		if c.Name == table.PrimaryColumn.Name && sqlTypeIsNumber && table.AutoIncrement > 0 {
+			stmt += " PRIMARY KEY"
+			if table.AutoIncrement > 0 {
+				stmt += " AUTOINCREMENT"
+			}
+		}
+		if c.DefaultValue != "" {
+			if sqlTypeIsNumber || c.DefaultValue == "NULL" {
+				stmt += " DEFAULT " + c.DefaultValue
+			} else {
+				stmt += fmt.Sprintf(" DEFAULT '%v'", c.DefaultValue)
+			}
+		} else if c.NotNull {
+			if sqlTypeIsText {
+				stmt += " DEFAULT ''"
+			} else if !columnIsPrimaryKey {
+				stmt += " DEFAULT 0"
+			}
+		} else if sqlTypeIsText || !columnIsPrimaryKey {
+			stmt += " DEFAULT NULL"
+		}
+
+		if sqlTypeIsText {
+			stmt += " COLLATE NOCASE"
+		}
+		stmt += ", \n"
+	}
+	if !strings.Contains(table.PrimaryColumn.SqlType, "int(") || table.AutoIncrement < 1 {
+		stmt += fmt.Sprintf("PRIMARY KEY (`%v`));\n", table.PrimaryColumn.Name)
+	} else {
+		stmt = stmt[:len(stmt)-3] + ");\n"
+	}
+	for _, c := range table.UniqueIndexes {
+		stmt += fmt.Sprintf("CREATE UNIQUE INDEX `U_%[1]v_%[2]v` ON `%[1]v` (`%[2]v`);\n", table.Name, c.Name)
+	}
+	for _, i := range table.Indexes {
+		columns := ""
+		for _, c := range i.Keys {
+			columns += fmt.Sprintf("`%v`, ", c.Name)
+		}
+		columns = columns[:len(columns)-2]
+		stmt += fmt.Sprintf("CREATE INDEX `%[2]v` ON `%[1]v` (%[3]v);\n", table.Name, i.Name, columns)
+	}
+	return stmt
 }
 
 type mysqlProvider struct {
@@ -121,4 +193,51 @@ func (provider *mysqlProvider) getPrimaryKeyColumn(tableName string) string {
 
 func (provider *mysqlProvider) closeConnection() {
 	provider.db.Close()
+}
+
+func (provider *mysqlProvider) getCreateStatement(table Table) string {
+	stmt := ""
+	if table.Static {
+		stmt += fmt.Sprintf("DROP TABLE IF EXISTS `%v`;\n\n", table.Name)
+		stmt += fmt.Sprintf("CREATE TABLE `%v` (\n", table.Name)
+	} else {
+		stmt += fmt.Sprintf("CREATE TABLE IF NOT EXISTS `%v` (\n", table.Name)
+	}
+	for _, k := range table.Columns {
+		stmt += "  `" + k.Name + "` " + k.SqlType
+		if k.NotNull {
+			stmt += " NOT NULL"
+		}
+		if k.DefaultValue != "" {
+			if k.DefaultValue == "NULL" {
+				stmt += " DEFAULT NULL"
+			} else {
+				stmt += " DEFAULT '" + k.DefaultValue + "'"
+			}
+		}
+		if (k == table.PrimaryColumn && table.AutoIncrement > 0) || k.AutoIncrement {
+			stmt += " AUTO_INCREMENT"
+		}
+		stmt += ",\n"
+	}
+	stmt += "  PRIMARY KEY (`" + table.PrimaryColumn.Name + "`),\n"
+	for _, k := range table.UniqueIndexes {
+		stmt += "  UNIQUE KEY `U_" + table.Name + "_" + k.Name + "` (`" + k.Name + "`),\n"
+	}
+
+	for _, i := range table.Indexes {
+		stmt += "  KEY `" + i.Name + "` ("
+		for _, k := range i.Keys {
+			stmt += "`" + k.Name + "`,"
+		}
+		stmt = stmt[:len(stmt)-1]
+		stmt += "),\n"
+	}
+	stmt = stmt[0:len(stmt)-2] + "\n"
+	stmt += ") ENGINE=InnoDB"
+	if table.AutoIncrement > 1 {
+		stmt += fmt.Sprintf(" AUTO_INCREMENT=%v", table.AutoIncrement)
+	}
+	stmt += " DEFAULT CHARSET utf8 COLLATE utf8_general_ci;\n"
+	return stmt
 }
