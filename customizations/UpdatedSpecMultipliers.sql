4bf9ae066ab575494cfee88414c942a05e41751d
tegstewart <tegstewart@gmail.com>
2023-02-02 10:59:17 -0700

    Create UpdatedSpecMultipliers.sql (#305)
    * Create UpdatedSpecMultipliers.sql
    
    Updates spec multiplier to match current live values.
    
    * Add CharacterClass Table
    
    ---------
    
    Co-authored-by: NetDwarf <NetDwarf@users.noreply.github.com>
    Co-authored-by: Lucas Falk <LRFalk01@gmail.com>

---
+++ customizations/UpdatedSpecMultipliers.sql
@@ -0,0 +1,8 @@
+# Updated specialization multipliers to match live
+
+REPLACE INTO `CharacterClass` (`ID`,`SpecPointMultiplier`,`CharacterClass_ID`) VALUES (9, 29, 'Infiltrator');
+REPLACE INTO `CharacterClass` (`ID`,`SpecPointMultiplier`,`CharacterClass_ID`) VALUES (23, 28, 'Shadowblade');
+REPLACE INTO `CharacterClass` (`ID`,`SpecPointMultiplier`,`CharacterClass_ID`) VALUES (49, 28, 'Nightshade');
+REPLACE INTO `CharacterClass` (`ID`,`SpecPointMultiplier`,`CharacterClass_ID`) VALUES (60, 18, 'MaulerAlb');
+REPLACE INTO `CharacterClass` (`ID`,`SpecPointMultiplier`,`CharacterClass_ID`) VALUES (61, 18, 'MaulerMid');
+REPLACE INTO `CharacterClass` (`ID`,`SpecPointMultiplier`,`CharacterClass_ID`) VALUES (62, 18, 'MaulerHib');
