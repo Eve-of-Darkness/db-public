1ca903bbdd92ddd84c04f22d0f126efb6359df35
tegstewart <tegstewart@gmail.com>
2023-02-10 02:21:43 -0700

    Changed to standard inline comments
    Was not aware # comments were a MySQL specific thing.  Oops.

--- customizations/UpdatedSpecMultipliers.sql
+++ customizations/UpdatedSpecMultipliers.sql
@@ -1,5 +1,5 @@
-# Updated specialization multipliers to match live
+-- Updated specialization multipliers to match live
 
-UPDATE `CharacterClass` SET `SpecPointMultiplier`=29 WHERE CharacterClass.ID IN (9); # Infiltrator
-UPDATE `CharacterClass` SET `SpecPointMultiplier`=28 WHERE CharacterClass.ID IN (23,49); # Shadowblade, Nightshade
-UPDATE `CharacterClass` SET `SpecPointMultiplier`=18 WHERE CharacterClass.ID IN (60,61,62); # Maulers
+UPDATE `CharacterClass` SET `SpecPointMultiplier`=29 WHERE CharacterClass.ID IN (9); -- Infiltrator
+UPDATE `CharacterClass` SET `SpecPointMultiplier`=28 WHERE CharacterClass.ID IN (23,49); -- Shadowblade, Nightshade
+UPDATE `CharacterClass` SET `SpecPointMultiplier`=18 WHERE CharacterClass.ID IN (60,61,62); -- Maulers
